4960704ea55ed4a5248e2bd0a32c37079dd01801
augusgerco <augustosgercovich@gmail.com>
2025-04-03 11:40:53 -0300

    Ejercicio 3 Terminado
    03.04.25

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,8 +12,8 @@ public class DatosDelUsuario : MonoBehaviour // aca se declaran las variables
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido" + estaturaUsuario + "metros y es" +
-           esDonante + "que soy donante de órganos.");
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido" + estaturaUsuario + "metros y es" 
+          + esDonante + "que soy donante de órganos.");
     }
 
     // Update is called once per frame
