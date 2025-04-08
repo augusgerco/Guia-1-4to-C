b2d840d46b2db849317bc04143e7d92a842fc636
augusgerco <augustosgercovich@gmail.com>
2025-04-08 09:05:02 -0300

    Ejercicio Practica Completo

---
+++ Assets/Scripts P2/SumaYProducto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumaYProducto : MonoBehaviour
+{
+    int num1 = 4;
+    int num2 = 5;
+    public int resultado = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log(resultado);
+        resultado = num1 * num2;
+        Debug.Log(resultado);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts Practica/ConversionDeMoneda.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeMoneda : MonoBehaviour
+{
+    //1. Declaración de variables
+    public float montoAConvertir;
+    public string codigoMoneda;
+
+    float montoConvertido;
+    float cotizacionMonedaElegida;
+
+    float montoMinimoAConvertir = 1000;
+    float cotizacionDolar = 1134.5f;
+    float cotizacionEuro = 1397.16f;
+    float cotizacionReal = 211.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2. Ingreso y validación de datos
+        if (montoAConvertir < montoMinimoAConvertir)
+        {
+            Debug.Log("El monto mínimo es " + montoMinimoAConvertir);
+            return; //hace q todo el codigo que este abajo no se ejecute
+        }
+
+        if (codigoMoneda == "D")  // las comparaciones se escriben con "=="
+        {
+            cotizacionMonedaElegida = cotizacionDolar;
+        } else if (codigoMoneda == "E")
+        {
+            cotizacionMonedaElegida = cotizacionEuro;
+        } else if (codigoMoneda == "R")
+        {
+            cotizacionMonedaElegida = cotizacionReal;
+        } else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+
+        //3. Operaciones y procesamiento de datos
+
+        montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+
+        //4. Salida de datos
+        Debug.Log("Monto convertido: " + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
